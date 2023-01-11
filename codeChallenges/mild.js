1c70a8eef23532ef992c993905f1318b9d68610a
Tiala Young <tialayoung@gmail.com>
2023-01-11 13:58:43 -0700

    Add challenge notes to file

--- codeChallenges/mild.js
+++ codeChallenges/mild.js
@@ -1,3 +1,8 @@
+// Write a JavaScript program that defines a variable that stores a Number.
+// The program should print out the String "even" if the Number is even,
+// and the String "odd" if the Number is odd.
+// Hint: You may need to do some extra research on the remainder operator (%) to solve this challenge.
+
 var count = -203;
 
 if (count % 2 === 1 || count % 2 === -1) {
