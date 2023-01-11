f3c137b2ec625ad70d64204080ad6a4e74786b59
Tiala Young <tialayoung@gmail.com>
2023-01-11 14:08:30 -0700

    Add second if statement with updated variable

--- codeChallenges/mild.js
+++ codeChallenges/mild.js
@@ -12,3 +12,13 @@ if (count % 2 === 1 || count % 2 === -1) {
 } else {
     console.log("something else")
 }
+
+count = 0;
+
+if ( count % 2 === 1 || count % 2 === -1) {
+    console.log("odd")
+} else if ( count % 2 === 0) {
+    console.log("even")
+} else { 
+    console.log("something else")
+}
