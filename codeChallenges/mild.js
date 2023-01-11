2b4bc1ef76eaa41b0046ac8ac18efbbccef1c543
Tiala Young <tialayoung@gmail.com>
2023-01-11 13:37:27 -0700

    Add completed challenge

---
+++ codeChallenges/mild.js
@@ -0,0 +1,10 @@
+var count = 7;
+
+if (count % 2 === 1 || count % 2 === -1) {
+    console.log("odd");
+} else if (count % 2 === 0 ) {
+    console.log("even");
+} else {
+    console.log("something else")
+}
+
